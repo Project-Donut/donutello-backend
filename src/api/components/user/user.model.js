976b905dd00f0c32d87a0e57e3d2e2593a47b071
GilianS <r0885351@student.thomasmore.be>
2022-11-28 23:38:11 +0100

    refactor: properly formatted role and user models

--- src/api/components/user/user.model.js
+++ src/api/components/user/user.model.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose");
 
-const User = mongoose.model(
-  "User",
-  new mongoose.Schema({
-    username: String,
-    email: String,
-    password: String,
-    roles: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Role"
-      }
-    ]
-  })
+const User = new mongoose.Schema(
+    {
+        username: String,
+        email: String,
+        password: String,
+        roles: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Role",
+            },
+        ],
+    },
+    { timestamps: true }
 );
 
-module.exports = User;
+module.exports = mongoose.model("User", User);
