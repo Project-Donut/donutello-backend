9606625fb9120654749daf9ef1c123a0608d8bb8
GilianS <r0885351@student.thomasmore.be>
2022-12-05 01:44:54 +0100

    fix: permissions now add unto each other (admin has all the rights of moderator, and moderator has all the rights of a user)

--- src/api/middleware/authJwt.js
+++ src/api/middleware/authJwt.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const User = require("../components/user/user.model");
 const Role = require("../components/role/role.model");
 const { ApiResult } = require("../JSend");
@@ -67,7 +68,7 @@ const isModerator = (req, res, next) => {
         }
 
         for (let i = 0; i < roles.length; i++) {
-          if (roles[i].name === "moderator") {
+          if (roles[i].name === "moderator" || roles[i].name === "admin") {
             next();
             return;
           }
@@ -79,6 +80,37 @@ const isModerator = (req, res, next) => {
     );
   });
 };
+
+const isUser = (req, res, next) => {
+    User.findById(req.userId).exec((err, user) => {
+      if (err) {
+        res.status(500).send({ message: err });
+        return;
+      }
+  
+      Role.find(
+        {
+          _id: { $in: user.roles }
+        },
+        (err, roles) => {
+          if (err) {
+            res.status(500).send({ message: err });
+            return;
+          }
+  
+          for (let i = 0; i < roles.length; i++) {
+            if (roles[i].name === "user" || roles[i].name === "moderator" || roles[i].name === "admin") {
+              next();
+              return;
+            }
+          }
+  
+          res.status(403).send({ message: "Require User Role!" });
+          return;
+        }
+      );
+    });
+  };
 
 module.exports = {
   verifyToken,
